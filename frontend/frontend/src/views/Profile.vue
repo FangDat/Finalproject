625222ee0789137233d4a487373d485f33155db0
FangDat <skyfall20192k4@gmail.com>
2026-02-22 21:47:53 +0700

    Update change email

--- frontend/frontend/src/views/Profile.vue
+++ frontend/frontend/src/views/Profile.vue
@@ -434,7 +434,7 @@ export default {
       // OTP
       showChangeEmailOtp: false,
       otpDigits: ["", "", "", "", "", ""],
-      resendTimer: 60,
+      resendTimer: 600,
       otpError: null,
       otpInterval: null,
       verifyingOtp: false,
@@ -628,7 +628,7 @@ export default {
   },
   
   startResendCountdown() {
-  this.resendTimer = 60;
+  this.resendTimer = 600;
   if (this.otpInterval) clearInterval(this.otpInterval);
   this.otpInterval = setInterval(() => {
     if (this.resendTimer > 0) this.resendTimer--;
